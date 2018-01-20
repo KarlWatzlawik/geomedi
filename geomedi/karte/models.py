30e411f92c39845c695fa2b1196da60208a1ba78
Cornel Remer <cornel.remer@gmail.com>
2018-01-20 13:38:07 +0100

    Quellen angegeben

--- geomedi/karte/models.py
+++ geomedi/karte/models.py
@@ -1,27 +1,14 @@
 from __future__ import unicode_literals
-#from django.db import models                #std db
 from django.contrib.gis.db import models     #postgresql db
 from djgeojson.fields import PointField
 
 class Marker(models.Model):
     name = models.CharField(max_length=50)
-    #picture = models.ImageField(default='')
     picture = models.CharField(max_length=100)
     geom = models.PointField(srid=4326, blank=True)
     objects = models.GeoManager()
     description = models.TextField(default='')
     author = models.TextField(default='')
-
-    #geom = models.MultiPolygonField(srid=21037)
-    
-    #def __str__(self):
-    #@property
-    #def popupContent(self):
-        #return "{}".format(self.picture)
-        #return '<img width="80" src="https://i.ytimg.com/vi/JfFP273mYQg/hqdefault.jpg" /><br/><b><{}</b>'.format(
-        #return '<img width="80" src="{}" /><br/><b><{}</b>'.format(
-            #self.picture.url,
-            #self.description)
 
     def __unicode__(self):
         return self.name
@@ -34,8 +21,6 @@ class Polygon(models.Model):
     geom = models.MultiPolygonField()
     objects = models.GeoManager()
     description = models.TextField(default='')
-
-    #geom = models.MultiPolygonField(srid=21037)
     
     def __unicode__(self):
         return self.name
--- geomedi/karte/templates/map/map.js
+++ geomedi/karte/templates/map/map.js
@@ -1,50 +1,63 @@
-function ourfunction (map, options){
-    
-    //Marker with GeoJson
-    var name;
+function ourfunction(map, options) {
+    //Marker mit Inhalten aus GeoJson
     function onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.picture) {
-        layer.bindPopup('<p>' + feature.properties.picture + '</p> <br/> <img src="{% static "map/images/Fernsehtrum.png" %}" /><p>' + feature.properties.description + '</p>');
-        sidebar.setContent('<b>' + feature.properties.name + '</b>');
+        //Popup-Inhalte für Point-Marker
+        if (feature.geometry.type === 'Point') {
+            layer.bindPopup('<h4 class="h4_pin">' + feature.properties.name + '</h4>' + '<img src="../static/map/images/sights/' + feature.properties.picture + '.jpg"/><br/>' + '<br><button class="button_pin trigger">mehr</button>');
+            //Bei klick auf Botten wird die Sidebar geöffnet
+            $('#map').on('click', '.trigger', function () {
+                show = true;
+                openSidebar();
+            });
+            layer.on({
+                click: openSidebar,
+            });
+        }
+        //Popup-Inhalte für Polygon-Marker
+        if (feature.geometry.type === 'MultiPolygon') {
+            layer.bindPopup(feature.properties.name);
+        }
+        //OnClick Sidebar ausbelden
+        map.on('click', function () {
+            sidebar.hide();
+        })
     }
 
-    //OnClick show Sidebar
-    layer.on('click', function (e) {
-        sidebar.toggle();
-    });
+    //SIDEBAR-FUNCTION
+    var show = false;       //boolean-wert zum aktivieren der sidebar
+    function openSidebar(e) {
+        if (show) {
+            sidebar.toggle();   //Anzeige der Sidebar
+            show = false;
+        }
+        if (e.target.feature.geometry.type === 'Point') {
+            sidebar.setContent('<h3 class="h3_sidebar">' + e.target.feature.properties.name + '</h3><br/>' + '<img src="../static/map/images/sights/' + e.target.feature.properties.picture + '.jpg"/><br/>' + '<p class="p_sidebar">' + e.target.feature.properties.description + '</p>');     //Sidebar-Inhalte
+        }
+    }
 
-    //OnClick hide Sidebar
-    map.on('click', function () {
-        sidebar.hide();
-    })
-}
-
+    //Layercontrol erstellen
     var controlLayers = L.control.layers().addTo(map);
 
-    //Add Marker
-    var ourdata = '{% url "data" %}';
-    
-    $.getJSON(ourdata, function(data){
-        var marker = L.geoJson(data,{onEachFeature: onEachFeature}).addTo(map);
-        controlLayers.addOverlay(marker, 'Marker');
+    //Marker zur Karte hinzufügen
+    var ourdata = '{% url "data" %}';                    //URL für Markerdaten im GeoJSON-Format
+
+    $.getJSON(ourdata, function (data) {
+        var marker = L.geoJson(data, {onEachFeature: onEachFeature}).addTo(map);
+        controlLayers.addOverlay(marker, 'Marker');      //erzeugt Layergrout "Marker"
     })
 
-    //Add Polygon
-    var polydata = '{% url "poly" %}';
-    
-    $.getJSON(polydata, function(poly){
-        var polygone = L.geoJson(poly).addTo(map);
-        controlLayers.addOverlay(polygone, 'Polygone');
+    //Polygone zur Karte hinzufügen
+    var polydata = '{% url "poly" %}';                   //URL für Polygondaten im GeoJSON-Format
+
+    $.getJSON(polydata, function (poly) {
+        var polygone = L.geoJson(poly, {onEachFeature: onEachFeature}).addTo(map);
+        controlLayers.addOverlay(polygone, 'Polygone');  //erzeugt Layergroup "Polygone"
     })
-    
-    //Routing
+    var start = false;  //Boolean Variable zum aktivieren der Sidebar
+
+    //########## ROUTING ##########
+    //Quelle: http://www.liedman.net/leaflet-routing-machine/tutorials/
     L.Routing.control({
-        //Startpunkte festlegen
-        waypoints: [
-            L.latLng(52.514, 13.350),
-            L.latLng(52.520, 13.409)
-        ],
-        //Lineoptions
         lineOptions: {
             styles: [
                 {color: 'black', opacity: 0.3, weight: 11},
@@ -52,27 +65,47 @@ function ourfunction (map, options){
                 {color: 'red', opacity: 1, weight: 3}
             ]
         },
-        //List collapse
+        routeWhileDragging: true,
+        //Geocoder Nominatim
+        geocoder: L.Control.Geocoder.nominatim(),
+        //Routing ausklappbar (true)
         collapsible: true,
-        //Geocoder
-        geocoder: true,
-        //show routing in beginning
+        //Routing zu Beginn nicht anzeigen (false)
         show: false,
-
+        //Sprachausgabe
+        language: 'de'
     }).addTo(map);
 
-    //Init Sidebar
+    ////########## SIDEBAR ##########
+    //Quelle: https://github.com/turbo87/leaflet-sidebar/
     var sidebar = L.control.sidebar('sidebar', {
         position: 'left',
         closeButton: 'true'
     });
-    //Start options: show/hide sidebar
+    //Start-Option: show/hide sidebar
     setTimeout(function () {
         sidebar.hide();
     }, 500);
 
     //Add content to sidebar
-    //sidebar.setContent('<b>' + test + '</b>');
     map.addControl(sidebar);
+}
 
- }
+////########## SLEEP MAP ##########
+//Quelle: https://github.com/CliffCloud/Leaflet.Sleep
+map3 = L.map('map', {
+    //Aktivieren der Sleep-Funktion
+    sleep: true,
+    //Timer (ms) bis zum erneuten Aktivieren der Sleep-Funktion
+    sleepTime: 750,
+    //Timer (ms) bis zum Erwachen der Karte bei Mouseover
+    wakeTime: 750,
+    //Zeigt Infos wie die Karte aktiviert werden kann
+    sleepNote: true,
+    //Erlaubnis die styling note zu überschreiben
+    sleepNoteStyle: {color: 'red'},
+    //Mouseover aktiviert die Karte (Klick geht immer)
+    hoverToWake: true,
+    //Transparenz bei deaktivierter Karte
+    sleepOpacity: .7
+})
